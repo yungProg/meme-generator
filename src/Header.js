93530e3b012e8d600e2564b663eb27b87e6b4a18
o-brown007 <gboakye97@yahoo.com>
2023-07-19 06:39:41 +0000

    completed header component

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src=".\troll-face.png" className="header--image" alt="troll face" />
+            <h1 className="header--title">Meme Generator</h1>
+            <h2 className="header--project-num">React Course - Project 3</h2>
+        </header>
+    )
+}
+
+export default Header
