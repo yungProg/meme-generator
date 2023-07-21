5a101c28566287abc80d3bb8f6e4d51608d873d7
o-brown007 <gboakye97@yahoo.com>
2023-07-21 15:47:50 +0000

    add meme images. yay!

--- src/Meme.js
+++ src/Meme.js
@@ -1,17 +1,29 @@
 import React from "react"
+import memeData from "./memeData";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faImage } from '@fortawesome/free-solid-svg-icons'
 
 const Meme = () => {
+
+    const memeUrls = memeData.data.memes.map(url => url.url);
+    let randomIndex = Math.floor(Math.random() * memeUrls.length)
+
+    const [meme, setMeme] = React.useState(memeUrls[0])
+
+    function handleMeme(){
+        setMeme(memeUrls[randomIndex])
+    }
+
     return (
         <section>
-            <form className="section--form">
+            <div className="section--form">
                 <div className="section--inputs">
                     <input type="text" placeholder="First line" />
                     <input type="text" placeholder="Second line" />
                 </div>
-                <button value="submit">Get a new meme image <FontAwesomeIcon icon={faImage} /></button> 
-            </form>
+                <button value="submit" onClick={handleMeme}>Get a new meme image <FontAwesomeIcon icon={faImage} /></button> 
+                <div className="div--meme"><img className="meme" src={meme} alt="meme" /></div>
+            </div>
         </section>
     )
 }
